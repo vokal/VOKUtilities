d174af9f8f744aa736966546a3a192921fd44206
Brock Boland <brock@brockboland.com>
2017-01-24 16:03:39 -0600

    Added UI tests for VOKNavigationHelper

---
+++ Example/UITests/VOKNavigationHelper_UITests.m
@@ -0,0 +1,42 @@
+//
+//  VOKNavigationHelper_UITests.m
+//  VOKUtilities
+//
+//  Created by Brock Boland on 1/24/17.
+//  Copyright © 2017 Vokal. All rights reserved.
+//
+
+#import <KIF/KIF.h>
+#import "VOKNavigationHelper.h"
+#import "AnotherViewController.h"
+#import "TestViewController.h"
+
+@interface VOKNavigationHelper_UITests : KIFTestCase
+
+@end
+
+@implementation VOKNavigationHelper_UITests
+
+- (void)afterAll
+{
+    // Reset back to the default view controller
+    [VOKNavigationHelper clearExistingViewsAndSwitchToViewController:[[TestViewController alloc] init]];
+    [super afterAll];
+}
+
+- (void)testStoryboardJumping
+{
+    [VOKNavigationHelper clearExistingViewsAndSwitchToStoryboardNamed:@"First"];
+    [tester waitForViewWithAccessibilityLabel:@"This is in the first storyboard"];
+    [VOKNavigationHelper clearExistingViewsAndSwitchToStoryboardNamed:@"Second"];
+    [tester waitForViewWithAccessibilityLabel:@"This is in the second storyboard"];
+}
+
+- (void)testViewControllerJumping
+{
+    UIViewController *viewController = [[AnotherViewController alloc] init];
+    [VOKNavigationHelper clearExistingViewsAndSwitchToViewController:viewController];
+    [tester waitForViewWithAccessibilityLabel:@"This is a label from code"];
+}
+
+@end
---
+++ Example/VOKUtilities/AnotherViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnotherViewController.h
+//  VOKUtilities
+//
+//  Created by Brock Boland on 1/24/17.
+//  Copyright © 2017 Vokal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnotherViewController : UIViewController
+
+@end
