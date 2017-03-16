2e2f5361f8754675cb8e15264bde420841bf8ebb
Ellen Shapiro <designatednerd@gmail.com>
2017-03-16 12:45:20 -0500

    pod install with cocoapods 1.2.0 final

--- Example/Pods/Target Support Files/VOKUtilities-5d04d789/VOKUtilities-5d04d789-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_VOKUtilities_5d04d789 : NSObject
-@end
-@implementation PodsDummy_VOKUtilities_5d04d789
-@end
--- Example/Pods/Target Support Files/VOKUtilities-dc5c4d91/VOKUtilities-dc5c4d91-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_VOKUtilities_dc5c4d91 : NSObject
-@end
-@implementation PodsDummy_VOKUtilities_dc5c4d91
-@end
