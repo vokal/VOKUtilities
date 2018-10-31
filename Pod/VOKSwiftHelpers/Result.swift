7470b8218769859d98b907c5794d72b43f04dbd9
Bryan Luby <bryanluby@users.noreply.github.com>
2018-10-31 14:31:33 -0500

    Add Swift testing helpers

--- Pod/VOKSwiftHelpers/Result.swift
+++ Pod/VOKSwiftHelpers/Result.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 /// Generic completion closure for a Result.
-typealias ResultCompletion<T> = (Result<T>) -> Void
+public typealias ResultCompletion<T> = (Result<T>) -> Void
 
 /// An enum representing a success or failure.
 ///
 /// - success: the success case with the associated value.
 /// - failure: the failure case with the associated error.
-enum Result<Value> {
+public enum Result<Value> {
     case success(Value)
     case failure(Error)
 }
