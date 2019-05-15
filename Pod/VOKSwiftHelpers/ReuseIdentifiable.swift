5eacf8c610e6ab6b5f7cfb4028c78924a3eef503
Bryan Luby <bryanluby@users.noreply.github.com>
2019-05-15 16:02:27 -0500

    Add ReuseIdentifiable protocol

---
+++ Pod/VOKSwiftHelpers/ReuseIdentifiable.swift
@@ -0,0 +1,36 @@
+//
+//  ReuseIdentifiable.swift
+//  VOKUtilities
+//
+//  Copyright © 2019 Vokal. All rights reserved.
+//
+
+import UIKit
+
+/// Mix-in protocol for cells and views that make use of a reuse identifier. The default implementation of
+/// `defaultReuseIdentifier` returns the class name. Add this protocol to a class to use that default functionality.
+/// For nibs, be sure to set the reuse identifier in Interface Builder to the class name.
+public protocol ReuseIdentifiable: class {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension ReuseIdentifiable where Self: UIView {
+    public static var defaultReuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+/// Provide a default reuse identifier for table view cells.
+extension UITableViewCell: ReuseIdentifiable {
+    // Mix-in
+}
+
+/// Provide a default reuse identifier for table view headers and footers.
+extension UITableViewHeaderFooterView: ReuseIdentifiable {
+    // Mix-in
+}
+
+/// Provide a default reuse identifier for collection view cells.
+extension UICollectionReusableView: ReuseIdentifiable {
+    // Mix-in
+}
