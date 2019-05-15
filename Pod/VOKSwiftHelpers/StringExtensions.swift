9e8c170da35031ee6fc267732001b22b0debc349
Bryan Luby <bryanluby@users.noreply.github.com>
2019-05-15 13:02:36 -0500

    Add initial Swift string extensions

---
+++ Pod/VOKSwiftHelpers/StringExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  StringExtensions.swift
+//  VOKUtilities
+//
+//  Copyright © 2019 Vokal. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    /// Get a new string by removing non-digits.
+    public var digits: String {
+        return self.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+    }
+    
+    /// Get the double value of a string. Will return nil if it can't convert.
+    public var doubleValue: Double? {
+        return Double(self)
+    }
+    
+    /// Get the int value of a string. Will return nil if it can't convert.
+    public var intValue: Int? {
+        return Int(self)
+    }
+}
