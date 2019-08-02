2e7ff61dbbd84d067d69f3967dccc0069671eae4
Bryan Luby <bryanluby@users.noreply.github.com>
2019-08-02 10:15:48 -0500

    Add attributed string extension convenience initializer

---
+++ Example/Tests/NSAttributedStringExtensionsTests.swift
@@ -0,0 +1,18 @@
+//
+//  NSAttributedStringExtensionsTests.swift
+//  VOKUtilities
+//
+//  Copyright © 2019 Vokal. All rights reserved.
+//
+
+import XCTest
+@testable import VOKUtilities
+
+class NSAttributedStringExtensionsTests: XCTestCase {
+    func testInitWithHTMLString() {
+        let HTMLString = #"<p>Test test <a href="https://www.example.com/">Example</a></p>"#
+        let attributes = [NSAttributedString.Key.foregroundColor: UIColor.blue]
+        let attributedString = NSAttributedString(HTMLString: HTMLString, attributes: attributes)
+        XCTAssertNotNil(attributedString)
+    }
+}
---
+++ Pod/VOKSwiftHelpers/NSAttributedStringExtensions.swift
@@ -0,0 +1,36 @@
+//
+//  NSAttributedStringExtensions.swift
+//  VOKUtilities
+//
+//  Copyright © 2019 Vokal. All rights reserved.
+//
+
+import UIKit
+
+extension NSAttributedString {
+    /// Create a new attributed string from an HTML string.
+    ///
+    /// This will configure and style an attributed string with an HTML document type.
+    /// - Parameter HTMLString: the attributed HTML String.
+    /// - Parameter attributes: the attributes to apply to the string. Default is empty.
+    convenience init?(HTMLString: String, attributes: [NSAttributedString.Key: Any] = [:]) {
+        let options = [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html]
+        
+        guard
+            // Need to use UTF-16 here as attributed strings apparently need data encoded that way.
+            let HTMLStringData = HTMLString.data(using: .utf16),
+            let attributedHTMLString = try? NSMutableAttributedString(data: HTMLStringData,
+                                                                      options: options,
+                                                                      documentAttributes: nil) else {
+                                                                        print("There was an issue with the HTML string")
+                                                                        return nil
+        }
+        
+        if !attributes.isEmpty {
+            let range = NSRange(location: 0, length: attributedHTMLString.length)
+            attributedHTMLString.addAttributes(attributes, range: range)
+        }
+        
+        self.init(attributedString: attributedHTMLString)
+    }
+}
